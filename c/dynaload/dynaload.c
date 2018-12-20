30d795064e812969433ff942460980b725ecd548
Lloyd Dilley <ldilley@users.noreply.github.com>
2018-12-20 03:29:55 -0500

    Add input validation and growing buffer

--- c/dynaload/dynaload.c
+++ c/dynaload/dynaload.c
@@ -1,9 +1,11 @@
 /* Demonstrates the loading and reloading of dynamic modules */
 
-#include <dlfcn.h>  /* dlopen(), dlerror(), dlsym(), dlclose() */
-#include <stdio.h>  /* fprintf(), printf(), fgets(), puts() */
-#include <stdlib.h> /* atoi(), exit() */
-#include <string.h> /* strcspn() */
+#include <dlfcn.h>   /* dlopen(), dlerror(), dlsym(), dlclose() */
+#include <stdbool.h>
+#include <stdint.h>  /* SIZE_MAX */
+#include <stdio.h>   /* fprintf(), printf(), fgets(), puts() */
+#include <stdlib.h>  /* atoi(), exit() */
+#include <string.h>  /* strcspn(), strcpy(), strlen() */
 
 /* Data types to determine function in assign_func() */
 enum data_type{INT, FLOAT, VOID};
@@ -20,6 +22,12 @@ void assign_func(lib_funcs *lf, void *lib_handle, enum data_type dt, const char 
 
 void load_lib(lib_funcs *lf, void **lib_handle, const char *lib_name)
 {
+  if(lf->sum)
+  {
+    printf("\nAlready loaded: %s\n", lib_name);
+    return;
+  }
+
   lib_handle = dlopen(lib_name, RTLD_NOW); /* attempt to open library */
   if(!lib_handle)
   {
@@ -34,17 +42,26 @@ void load_lib(lib_funcs *lf, void **lib_handle, const char *lib_name)
   assign_func(lf, lib_handle, FLOAT, "half");
   assign_func(lf, lib_handle, VOID, "print_message");
 
+  printf("\nLoaded: %s\n", lib_name);
+
   return;
 }
 
-void unload_lib(lib_funcs *lf, void **lib_handle)
+void unload_lib(lib_funcs *lf, void **lib_handle, const char *lib_name)
 {
+  if(!lf->sum)
+  {
+    printf("\nAlready unloaded: %s\n", lib_name);
+    return;
+  }
+
   if(lib_handle)
     dlclose(&lib_handle);
   lib_handle = NULL;
   lf->sum = NULL;
   lf->half = NULL;
   lf->print_message = NULL;
+  printf("\nUnloaded: %s\n", lib_name);
   return;
 }
 
@@ -80,6 +97,47 @@ void assign_func(lib_funcs *lf, void *lib_handle, enum data_type dt, const char 
   return;
 }
 
+/* Automatically expands to accommodate user input. Caller must free() returned pointer. */
+char *get_input()
+{
+  char input[64];
+  char *new_input = NULL;
+  char *tmp_input = NULL;
+  size_t input_length = 0;
+  size_t full_length = 0;
+
+  while(fgets(input, sizeof(input), stdin))
+  {
+    input_length = strlen(input);
+    if(SIZE_MAX - input_length - 1 < full_length)
+      break;
+
+    tmp_input = realloc(new_input, full_length + input_length + 1);
+    if(tmp_input == NULL)
+      break;
+    new_input = tmp_input;
+
+    strcpy(new_input + full_length, input);
+    full_length += input_length;
+    if(feof(stdin) || input[input_length-1] == '\n')
+      return new_input;
+  }
+  free(new_input);
+  return "";
+}
+
+/* Ensure input is numeric except for trailing character */
+bool validate_input(char *input)
+{
+  int i = 0;
+  for(i = 0; i < strlen(input) - 1; i++)
+  {
+    if(!isdigit(input[i]))
+      return false;
+  }
+  return true;
+}
+
 /* Automatically test the library functions */
 void auto_test_lib(lib_funcs *lf)
 {
@@ -98,7 +156,7 @@ void auto_test_lib(lib_funcs *lf)
 /* Manually test the library functions */
 void man_test_lib(lib_funcs *lf)
 {
-  char input[65];
+  char *input = "";
   int x = 0, y = 0;
 
   if(lf->sum)
@@ -106,24 +164,27 @@ void man_test_lib(lib_funcs *lf)
     puts("Notice: Invalid input will be considered zero.");
 
     printf("Enter a number (x) to sum: ");
-    fgets(input, sizeof(input) / sizeof(char), stdin);
+    input = get_input();
     x = atoi(input);
+    free(input);
     printf("Enter another number (y) to sum: ");
-    fgets(input, sizeof(input) / sizeof(char), stdin);
+    input = get_input();
     y = atoi(input);
+    free(input);
     printf("sum(%d, %d) = %d\n\n", x, y, lf->sum(x, y));
 
     printf("Enter a number to half: ");
-    fgets(input, sizeof(input) / sizeof(char), stdin);
+    input = get_input();
     x = atoi(input);
+    free(input);
     printf("half(%d) = %.2f\n\n", lf->half(x));
 
-    printf("Enter a string to print (%d characters max): ", (sizeof(input) / sizeof(char)) - 1);
-    /* ToDo: Protect against going over input size */
-    fgets(input, sizeof(input) / sizeof(char), stdin);
+    printf("Enter a string to print: ");
+    input = get_input();
     input[strcspn(input, "\n")] = 0; /* remove trailing newline */
     printf("print_message(\"%s\") = ", input);
     lf->print_message(input);
+    free(input);
   }
   else
     puts("Library not loaded!");
@@ -132,8 +193,7 @@ void man_test_lib(lib_funcs *lf)
 
 void menu(lib_funcs *lf, void **lib_handle, const char *lib_name)
 {
-  char input[64];
-  int c = 0;
+  char *input = "";
 
   while(atoi(input) != 5)
   {
@@ -145,14 +205,17 @@ void menu(lib_funcs *lf, void **lib_handle, const char *lib_name)
     printf("4. Manually test %s functions\n", lib_name);
     puts("5. Quit\n");
     printf("> ");
-    /* ToDo: Protect against going over input size */
-    fgets(input, sizeof(input) / sizeof(char), stdin);
-    input[2] = '\0';
-    input[1] = '\n';
+    input = get_input();
+    if(!validate_input(input))
+    {
+      puts("\nPlease enter a valid number!");
+      free(input);
+      continue;
+    }
     switch(atoi(input))
     {
       case 1:
-        unload_lib(lf, lib_handle);
+        unload_lib(lf, lib_handle, lib_name);
         break;
       case 2:
         if(lib_handle)
@@ -169,6 +232,7 @@ void menu(lib_funcs *lf, void **lib_handle, const char *lib_name)
         break;
       case 5:
         dlclose(&lib_handle);
+        free(input);
         puts("\nGoodbye!\n");
         exit(0);
         break;
@@ -176,6 +240,7 @@ void menu(lib_funcs *lf, void **lib_handle, const char *lib_name)
         puts("\nPlease enter a valid number!");
         break;
     }
+    free(input);
   }
   return;
 }
