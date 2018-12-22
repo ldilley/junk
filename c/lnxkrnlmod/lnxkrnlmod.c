8c11634d718bca1609af717709392d24d62dd936
Lloyd Dilley <ldilley@users.noreply.github.com>
2018-12-22 03:48:40 -0500

    Add lnxkrnlmod_sum()

--- c/lnxkrnlmod/lnxkrnlmod.c
+++ c/lnxkrnlmod/lnxkrnlmod.c
@@ -11,6 +11,13 @@ MODULE_DESCRIPTION("A demonstrational kernel module");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("1.0");
 
+static void lnxkrnlmod_sum(int x, int y)
+{
+  printk("%s->%s(%d, %d): %d + %d = %d\n", MODULE_NAME, __func__, x, y, x, y, x + y);
+  return;
+}
+EXPORT_SYMBOL(lnxkrnlmod_sum); /* allows function to be called by other modules */
+
 static int __init lnxkrnlmod_init(void)
 {
   printk(KERN_INFO "%s->%s(): Kernel module loaded.\n", MODULE_NAME, __func__);
